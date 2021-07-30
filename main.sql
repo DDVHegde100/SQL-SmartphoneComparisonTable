c305dccb8bfb5b93cefd18d2976c964befeb0f1a
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-07-30 08:51:25 -0400

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,12 @@
+create table phone_details (name varchar2(100), 
+description varchar2(255), 
+availability char, 
+price float, 
+in_stock int, 
+rating float);
+
+insert into phone_details values ('Samsung S21 Ultra', 'Samsung Flagship', 'y', 1250, 3, 4.5)
+insert into phone_details values ('Xiaomi Mi 11 Ultra', 'Xiaomi Flagship', 'y', 1200, 3, 5)
+insert into phone_details values ('Apple iPhone 12 Pro Max', 'Apple Flagship', 'y', 1100, 3, 0.5)
+insert into phone_details values ('Vivo X60 Pro Plus', 'Vivo Flagship', 'y', 1349, 3, 4)
+insert into phone_details values ('Oppo Find X3 Pro', 'Oppo Flagship', 'y', 1300, 3, 3.5)
